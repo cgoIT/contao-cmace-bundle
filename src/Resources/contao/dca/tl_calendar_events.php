22cf92b16eed04f410a85315333b7317424f8017
Carsten Götzinger <carsten.goetzinger@concedro.com>
2023-10-31 10:57:24 +0100

    feat: add option to link a news entry to an event

---
+++ src/Resources/contao/dca/tl_calendar_events.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of cgoit\contao-cmace-bundle.
+ *
+ * (c) Carsten Götzinger
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+use Contao\CoreBundle\DataContainer\PaletteManipulator;
+
+/*
+ * Extend palettes
+ */
+PaletteManipulator::create()
+    ->addLegend('news_legend', 'enclosure_legend', PaletteManipulator::POSITION_AFTER)
+    ->addField('news', 'news_legend', PaletteManipulator::POSITION_APPEND)
+    ->applyToPalette('default', 'tl_calendar_events')
+;
+
+/*
+ * Table tl_calendar_events
+ */
+$GLOBALS['TL_DCA']['tl_calendar_events']['fields'] = array_merge(
+    ['news' => [
+        'label' => &$GLOBALS['TL_LANG']['tl_calendar_events']['news'],
+        'inputType' => 'picker',
+        'filter' => false,
+        'exclude' => true,
+        'search' => false,
+        'relation' => ['type' => 'hasOne', 'load' => 'lazy', 'table' => 'tl_news'],
+        'eval' => ['mandatory' => false, 'tl_class' => 'clr w50'],
+        'sql' => 'int(10) unsigned NOT NULL default 0',
+    ]],
+    $GLOBALS['TL_DCA']['tl_calendar_events']['fields']
+);
---
+++ src/Resources/contao/languages/de/tl_calendar_events.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of cgoit\contao-bfv-widget-bundle.
+ *
+ * (c) Carsten Götzinger
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+$GLOBALS['TL_LANG']['tl_calendar_events']['news_legend'] = 'Verknüpfter News-Beitrag';
+
+$GLOBALS['TL_LANG']['tl_calendar_events']['news']['0'] = 'News-Artikel';
+$GLOBALS['TL_LANG']['tl_calendar_events']['news']['1'] = 'Der verknüpfte News-Beitrag zu diesem Event';
